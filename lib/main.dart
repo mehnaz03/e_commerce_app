2854c56d3f2d4ad1da4a8cce29bb732114e2143f
mehnaz03 <mehnazuiu@gmail.com>
2021-02-02 13:51:12 +0600

    change in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -5,87 +7,102 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'e-Shopping',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.orange,
-        ),
-        home: SplashScreen()
+      debugShowCheckedModeBanner: false,
+        home:LandingPage(),
+
     );
   }
+
 }
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-
-class _SplashScreenState extends State<SplashScreen>
-{
+class LandingPage extends StatelessWidget{
+  final Future<FirebaseApp>_initilization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text(
-          "Welcome to Flutter Firestore eCommerce by Mehnaz.",
-          style: TextStyle(color: Colors.deepOrange, fontSize: 20.0),
-          textAlign: TextAlign.center,
-        ),
-      ),
+    return FutureBuilder(
+      future: _initilization,
+      builder:(context,snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+              body: Center(
+                child: Text("Error:${snapshot.error}"),
+
+              )
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: Container(
+              child: Center(
+                child: Text("FireBase APP Initialized"),
+              ),
+            ),
+          );
+        }
+         return Scaffold(
+           body: Center(
+            child: Text("Initializing APP...."),
+          ),
+        );
+
+      },
     );
   }
+
 }
+//
 
 // class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//         home:LandingPage(),
-//
+//         title: 'e-Shopping',
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(
+//           primaryColor: Colors.orange,
+//         ),
+//         home: SplashScreen()
 //     );
 //   }
-//
 // }
 //
-// class LandingPage extends StatelessWidget{
-//   final Future<FirebaseApp>_initilization = Firebase.initializeApp();
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+//
+// class _SplashScreenState extends State<SplashScreen>
+// {
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     displaySplash();
+//   }
+//     displaySplash() {
+//      Timer(Duration(seconds: 5), () async{
+//
+//      });
+//   }
+//
 //   @override
 //   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       future: _initilization,
-//       builder:(context,snapshot) {
-//         if (snapshot.hasError) {
-//           return Scaffold(
-//               body: Center(
-//                 child: Text("Error:${snapshot.error}"),
-//
-//               )
-//           );
-//         }
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return Scaffold(
-//             body: Container(
-//               child: Center(
-//                 child: Text("FireBase APP Initialized"),
-//               ),
-//             ),
-//           );
-//         }
-//          return Scaffold(
-//            body: Center(
-//             child: Text("Initializing APP...."),
-//           ),
-//         );
-//
-//       },
+//     return Material(
+//       child: Center(
+//         child: Text(
+//           "Welcome to Flutter Firestore eCommerce by Mehnaz.",
+//           style: TextStyle(color: Colors.deepOrange, fontSize: 20.0),
+//           textAlign: TextAlign.center,
+//         ),
+//       ),
 //     );
 //   }
 //
-// }
 //
+// }
+
